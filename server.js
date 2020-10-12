796b130c81d055a639a03e4ab34b1e8ade48fb1a
Zettsuen <61870005+Zettsuen@users.noreply.github.com>
2020-10-12 21:30:50 +0200

    Server main file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
